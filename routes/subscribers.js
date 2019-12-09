b3b9aff055f512053c0a26f03f3e1c3d8243bfd8
othaderek <otha.hernandez@gmail.com>
2019-12-09 12:36:02 -0500

    Created Subscriber model, added to routes, created getSubscriber middleware in routes/subscriber.js

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,17 +1,46 @@
 const express = require('express')
 const router = express.Router()
+const Subscriber = require('../models/subscriber')
+
+const getSubscriber = async (req, res, next) => {
+    try {
+        subscriber = await Subscriber.findById(req.params.id)
+        if (subscriber == null) {
+            return res.status(404).json({ message: 'Cannot find subscriber' })
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
+
+    res.subscriber = subscriber
+    next()
+}
 
 // Getting all
-router.get('/', (req, res) => {
-    res.send('Hello World')
+router.get('/', async (req, res) => {
+    try {
+        const subscribers = await Subscriber.find()
+        res.json(subscribers)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
 })
 // Getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id)
+router.get('/:id', getSubscriber, (req, res) => {
+    res.send(res.subscriber.name)
 })
 // Creating One
-router.post('/', (req, res) => {
-    
+router.post('/', async (req, res) => {
+    const subscriber = new Subscriber({
+        name: req.body.name,
+        subscribedToChannel: req.body.subscribedToChannel
+    })
+    try {
+        const newSubscriber = await subscriber.save()
+        res.status(201).json(newSubscriber)
+    } catch (error) {
+        res.status(400).json({message: error.message})
+    }
 })
 // Updating one
 router.patch('/:id', (req, res) => {
@@ -21,6 +50,8 @@ router.patch('/:id', (req, res) => {
 router.delete('/:id', (req, res) => {
     
 })
+
+
 
 
 module.exports = router
