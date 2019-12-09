ea9723d08ed9cc7369fb8ce5a546813cd894742d
othaderek <otha.hernandez@gmail.com>
2019-12-09 11:31:54 -0500

    Got server running and connected to mongo via mongoose

---
+++ routes/subscribers.js
@@ -0,0 +1,26 @@
+const express = require('express')
+const router = express.Router()
+
+// Getting all
+router.get('/', (req, res) => {
+    res.send('Hello World')
+})
+// Getting one
+router.get('/:id', (req, res) => {
+    res.send(req.params.id)
+})
+// Creating One
+router.post('/', (req, res) => {
+    
+})
+// Updating one
+router.patch('/:id', (req, res) => {
+    
+})
+// Deleting one
+router.delete('/:id', (req, res) => {
+    
+})
+
+
+module.exports = router
