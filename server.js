a7524e51f59c19f9fe9184b6bb9fe964f1169d7c
othaderek <otha.hernandez@gmail.com>
2019-12-10 09:10:54 -0500

    added cors

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
 
@@ -11,6 +12,7 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 
 app.use(express.json())
+app.use(cors())
 
 const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
