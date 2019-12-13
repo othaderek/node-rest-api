4d2023482b58c506710863cdfb6c671db262a49e
othaderek <otha.hernandez@gmail.com>
2019-12-13 14:17:46 -0500

    Added chalk and styled two of the console messages.

--- server.js
+++ server.js
@@ -3,13 +3,14 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const chalk = require('chalk')
 const cors = require('cors')
 
 mongoose.connect(process.env.DATABASE_URL, { useUnifiedTopology: true, useNewUrlParser: true })
 
 const db = mongoose.connection 
 db.on('error', (error) => console.error(error))
-db.once('open', () => console.log('Connected to Database'))
+db.once('open', () => console.log(chalk.bold.inverse.green(' ~~ Connected to Database ~~ ')))
 
 app.use(express.json())
 app.use(cors())
@@ -18,7 +19,7 @@ const subscribersRouter = require('./routes/subscribers')
 app.use('/subscribers', subscribersRouter)
 
 app.listen(3000, () => {
-    console.log('Server Started')
+    console.log(chalk.bold.inverse.green(' ~~ Server Started ~~ '))
 });
 
 
